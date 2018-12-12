fbce0e43cf8f1ba1874f97e26f2a552120a128bb
Darya Shogol <da18121996@yandex.ru>
2018-12-12 13:18:38 +0300

    Миграции для сортировки

--- АРМ/Properties/Resources.Designer.cs
+++ АРМ/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace АРМ.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap search {
+            get {
+                object obj = ResourceManager.GetObject("search", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap user_4 {
             get {
                 object obj = ResourceManager.GetObject("user-4", resourceCulture);
